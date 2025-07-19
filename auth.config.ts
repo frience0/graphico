330b37ad496b59bdbe77474ab855b064c9eab9e0
frience0 <marckfrience0@gmail.com>
2025-07-19 15:14:47 +0800

    refactor auth.config.ts to add type annotation for authorized callback

--- auth.config.ts
+++ auth.config.ts
@@ -1,10 +1,12 @@
 import type { NextAuthConfig } from "next-auth";
 
+// Notice this is only an object, not a full Auth.js instance
 export default {
   trustHost: false,
   providers: [],
   callbacks: {
-    authorized({ request, auth }) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    authorized({ request, auth }: any) {
       const protectedPaths = [
         /\/checkout(\/.*)?/,
         /\/account(\/.*)?/,
